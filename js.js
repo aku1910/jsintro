ffd27ed048bc84c593dc256eca7e12bb794427d3
aku1910 <154792762+aku1910@users.noreply.github.com>
2024-02-04 22:00:13 -0800

    first commit

---
+++ js.js
@@ -0,0 +1,117 @@
+// // TASK1
+
+// var eded=3333;
+// var c="";
+// var arr=[];
+// while(eded>=1){
+//     var d=eded%10;
+// eded=(eded-eded%10)/10;
+// arr.push[d];
+// for(i=arr.length;i>=0;i--){
+//     c=c+d;
+// }
+// }
+// var d=parseInt(c);
+// var num2=d*10+700000+8
+// console.log(num2);
+
+
+//TASK2
+
+// var sum=0;
+// for(i=0;i<=1000;i++){
+// while(i>=1){
+//     var a=i%10;
+//     a=Math.floor(a);
+// i=(i-i%10)/10;
+//     sum=sum+a
+// }
+// if(sum%3==0){
+//     console.log(sum)
+// }
+// }
+
+
+// // TASK3
+
+// var a=1000;
+// var b=1000000;
+// console.log((a*0.04+b*0.09)*0.1);
+
+// // TASK4
+
+// var qapien=4;
+// var qapihundurluk=5;
+// var skafhundundurluk=20;
+// var skafen=3;
+// var skafuzunluq=3;
+// if(qapien<skafhundurluk || qapien<skafen || qapien<skafuzunluq){
+//     console.log("kecmez");
+// }
+// else if(qapihundurluk<skafhundurluk || qapihundurluk<skafen||qapihundurluk<skafuzunluq){
+//    console.log("kecmez");
+// }
+
+
+// else{
+//     console.log("kecer")
+
+
+
+// // TASK 5
+
+// var eded=1231;
+//  var d=eded%10;
+//  switch (d) {
+//     case 1:
+//         console.log("1ci gun")
+//         break;
+//         case 2:
+//             console.log("2ci gun")
+//             break;
+//             case 3:
+//             console.log("3cu gun")
+//             break;
+//             case 4:
+//             console.log("4cu gun")
+//             break;
+//             case 5:
+//             console.log("5ci gun")
+//             break;
+//             case 6:
+//             console.log(" 6ci gun")
+//             break;
+//             case 7:
+//             console.log("7ci gun")
+//             break;
+//     default:
+//         console.log("nothing");
+//         break;
+//  }
+
+// // TASK 6
+// var a =25;
+// var b =20;
+// var c =15;
+// var d= 32;
+// var e=42;
+// var f= 7;
+// if(a>b && a>c && a>d && a>e && a>f){
+//     console.log("a en boyukdur")
+// }
+// else if(b>a && b>c && b>d && b>e && b>f){
+//     console.log("b en boyukdur")
+// }
+// else if(c>a && c>b && c>d && c>e && c>f){
+//     console.log("c en boyukdur")
+// }
+// else if(d>a && d>b && d>c && d>e && d>f){
+//     console.log("d en boyukdur")
+// }
+// else if(e>a && e>b && e>c && e>d && e>f){
+//     console.log("e en boyukdur")
+// }
+// else{
+//     console.log("f en boyukdur")
+// }
+
